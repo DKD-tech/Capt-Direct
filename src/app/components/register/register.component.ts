573ac7e850600d8240c8bf0edffdf8dece926588
dkd-tech <diakitedjenebakaba@gmail.com>
2024-10-29 16:32:48 +0100

    UI Capt'Direct

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  constructor(private router: Router) {}
 
+  onRegister() {
+    // Logique d'inscription ici (par exemple, envoyer les données du formulaire à une API)
+    console.log('Inscription effectuée');
+    // Après l'inscription, rediriger vers la page de connexion ou un tableau de bord
+    this.router.navigate(['/dashboard-page']);
+  }
+  onLogin() {
+    this.router.navigate(['/login-page']);
+  }
 }
