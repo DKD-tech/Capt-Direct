e007670eb90543fd590caf8cdbb37149fd5b9017
asmakhoualdia98 <asmakhoualdia208@gmail.com>
2024-11-20 13:01:38 +0100

    Add videos-list component and update related files

--- captDirect/src/app/videos-list/videos-list.component.ts
+++ captDirect/src/app/videos-list/videos-list.component.ts
@@ -11,10 +11,10 @@ import { Component } from '@angular/core';
 export class VideosListComponent {
 
   videos = [
-    { title: 'Video 1', channel: 'Channel A', views: '1M views', time: '1 day ago' },
-    { title: 'Video 2', channel: 'Channel B', views: '500K views', time: '2 days ago' },
-    { title: 'Video 3', channel: 'Channel C', views: '200K views', time: '3 days ago' },
-    // Add more video data as needed
+    { title: 'Live Stream 1', channel: 'Channel A', views: '10K watching', time: '', isLive: true },
+    { title: 'Video 2', channel: 'Channel B', views: '500K views', time: '2 days ago', isLive: false },
+    { title: 'Live Stream 3', channel: 'Channel C', views: '25K watching', time: '', isLive: true },
+    { title: 'Video 4', channel: 'Channel D', views: '1M views', time: '1 week ago', isLive: false }
   ];
 
 }
