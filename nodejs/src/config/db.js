812a569f72e8069dcda46c974633995896cdeb71
dkd-tech <diakitedjenebakaba@gmail.com>
2025-04-01 17:26:46 +0200

    code fonctionnel

--- nodejs/src/config/db.js
+++ nodejs/src/config/db.js
@@ -1,15 +1,22 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+const isProd = process.env.DATABASE_URL !== undefined;
 // Utilisation des variables d'environnement de Railway avec SSL activé
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const pool = isProd
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      ssl: false, // ← désactive le SSL en local
+    });
 
-// Gestion des erreurs de connexion
 pool.on("error", (err) => {
   console.error("Erreur de connexion PostgreSQL :", err);
 });
